2a3b01e7b8f5b360b5815ba10bc43ad2e20b697c
Tom Heslop <t_heslop@hotmail.co.uk>
2015-10-03 15:55:54 +0100

    progress with collection page

--- traders-conference/example/R/collect-data.R
+++ traders-conference/example/R/collect-data.R
@@ -1,5 +1,6 @@
 #===============================================================================
 #                   RETRIEVE DATA FOR ATP TENNIS PLAYERS                       #
+#  see durtal.github.io/talks/traders-conference/example/data-collection.html  #
 #===============================================================================
 
 # LIBRARIES
@@ -8,7 +9,3 @@
 library(jsonlite)
 
 # data from players can be collected from their player pages on www.atpworldtour.com
-
-# may require finding the players url, this can be retrieve by entering the players
-# name into the search bar, or at the end of the url below, with andy murray as an example
-# 'http://www.atpworldtour.com/-/ajax/playersearch/PlayerUrlSearch?SearchTerm=andy murray'
